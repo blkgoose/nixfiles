17ea10cc7cceb62e08e2e1e24018a4a752f60b15
blkgoose <alessio.biancone@gmail.com>
2023-07-09 00:48:23 +0200

    fix(neovim): specify needed file one by one

--- home/modules/neovim.nix
+++ home/modules/neovim.nix
@@ -1,9 +1,10 @@
 { pkgs, lib, dots, config, ... }:
 {
-  home.file.".config/nvim" = {
-    source = "${dots}/nvim";
-    recursive = true;
-  };
+  xdg.configFile."nvim/init.lua".source = "${dots}/nvim/init.lua";
+  xdg.configFile."nvim/lua/config".source = "${dots}/nvim/lua/config";
+  xdg.configFile."nvim/lua/palette.lua".source = "${dots}/nvim/lua/palette.lua";
+  xdg.configFile."nvim/lua/plugins.lua".source = "${dots}/nvim/lua/plugins.lua";
+  xdg.configFile."nvim/lua/utils.lua".source = "${dots}/nvim/lua/utils.lua";
 
   programs.neovim = {
     enable = true;
