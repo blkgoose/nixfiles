3bc920ef184eb7627ae4bc9bfef62aebc4213166
blkgoose <alessio.biancone@gmail.com>
2023-07-09 11:32:28 +0200

    refactor(neovim): link lua folder

--- home/modules/neovim.nix
+++ home/modules/neovim.nix
@@ -1,10 +1,7 @@
 { pkgs, lib, dots, config, ... }:
 {
   xdg.configFile."nvim/init.lua".source = "${dots}/nvim/init.lua";
-  xdg.configFile."nvim/lua/config".source = "${dots}/nvim/lua/config";
-  xdg.configFile."nvim/lua/palette.lua".source = "${dots}/nvim/lua/palette.lua";
-  xdg.configFile."nvim/lua/plugins.lua".source = "${dots}/nvim/lua/plugins.lua";
-  xdg.configFile."nvim/lua/utils.lua".source = "${dots}/nvim/lua/utils.lua";
+  xdg.configFile."nvim/lua/".source = "${dots}/nvim/lua/";
 
   programs.neovim = {
     enable = true;
