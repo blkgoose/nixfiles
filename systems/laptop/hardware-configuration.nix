c86bd1d4b20861ce795d7b9d4c6010d2e48c76aa
blkgoose <alessio.biancone@gmail.com>
2023-07-17 13:34:18 +0200

    feat: use ondemand cpu governor

--- systems/laptop/hardware-configuration.nix
+++ systems/laptop/hardware-configuration.nix
@@ -25,7 +25,7 @@
   networking.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
+  powerManagement.cpuFreqGovernor = lib.mkDefault "ondemand";
   hardware.cpu.intel.updateMicrocode =
     lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
