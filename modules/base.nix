73afd46e0853c552384d7996fb6fdb8bf7559bfc
blkgoose <alessio.biancone@gmail.com>
2023-07-17 13:38:05 +0200

    feat: add htop

--- modules/base.nix
+++ modules/base.nix
@@ -7,6 +7,8 @@
   time.timeZone = "Europe/Rome";
   i18n.defaultLocale = "en_US.UTF-8";
 
+  environment.systemPackages = with pkgs; [ htop ];
+
   fonts.fonts = with pkgs; [
     font-awesome
     (nerdfonts.override { fonts = [ "Hasklig" ]; })
