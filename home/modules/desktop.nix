c3224703277e3b288149d07db48417934e01c604
blkgoose <alessio.biancone@gmail.com>
2023-07-19 09:27:22 +0200

    fix: remove picom blur

--- home/modules/desktop.nix
+++ home/modules/desktop.nix
@@ -25,25 +25,25 @@
   services.dunst.enable = true;
   services.picom = {
     enable = true;
-    backend = "glx";
+    # backend = "glx";
     vSync = true;
     # fade = true;
-    inactiveOpacity = 0.8;
+    # inactiveOpacity = 0.8;
 
-    settings = {
-      blur = {
-        method = "dual_kawase";
-        strength = 7.0;
-        deviation = 1.0;
-        kernel = "11x11gaussian";
-      };
-      blur-background = false;
-      blur-background-frame = true;
-      blur-background-fixed = true;
+    # settings = {
+    #   blur = {
+    #     method = "dual_kawase";
+    #     strength = 7.0;
+    #     deviation = 1.0;
+    #     kernel = "11x11gaussian";
+    #   };
+    #   blur-background = false;
+    #   blur-background-frame = true;
+    #   blur-background-fixed = true;
 
-      blur-background-exclude = [ "class_g = 'Chrome'" "name = 'slop'" ];
+    #   blur-background-exclude = [ "class_g = 'Chrome'" "name = 'slop'" ];
 
-      corner-radius = 10.0;
-    };
+    #   corner-radius = 10.0;
+    # };
   };
 }
