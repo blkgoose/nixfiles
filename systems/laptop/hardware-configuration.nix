3a0dafaae8246e941d60e2cde3f43abae526e52b
blkgoose <alessio.biancone@gmail.com>
2023-07-20 11:34:41 +0200

    feat: powersave governor

--- systems/laptop/hardware-configuration.nix
+++ systems/laptop/hardware-configuration.nix
@@ -25,7 +25,7 @@
   networking.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  powerManagement.cpuFreqGovernor = lib.mkDefault "ondemand";
+  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
   hardware.cpu.intel.updateMicrocode =
     lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
