fc9c645d10b92394e5832350700874eb8d2affe6
blkgoose <alessio.biancone@gmail.com>
2023-10-10 17:20:00 +0200

    feat: add beekeeper

--- home/modules/aliases.nix
+++ home/modules/aliases.nix
@@ -1,8 +1,12 @@
-{ lib, ... }:
+{ lib, pkgs, ... }:
 let
   apps = {
     "screenshot" = "escrotum --select --clipboard";
     "chrome" = "google-chrome-stable";
+    "beekeeper" = pkgs.writers.writeBash "beekeeper" ''
+      ${pkgs.sqlite}/bin/sqlite3 ~/.config/beekeeper-studio/app.db "UPDATE saved_connection SET host = 'postgres-$QA_HASH.prima.qa' WHERE name = 'QA'"
+      ${pkgs.beekeeper-studio}/bin/beekeeper-studio
+    '';
   };
 
   mkAlias = name: command: {
