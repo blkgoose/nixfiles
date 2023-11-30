e739b1d888ca3de2e7903354eba43de5119caf3f
blkgoose <alessio.biancone@gmail.com>
2023-11-30 17:16:47 +0100

    feat: update orca-slicer

--- modules/orca-slicer.nix
+++ modules/orca-slicer.nix
@@ -1,18 +1,30 @@
 { pkgs, ... }:
 let
-  version = "1.7.0";
+  version = "1.8.0";
   name = "OrcaSlicer_Linux_V${version}";
 in {
   environment.systemPackages = [
     (pkgs.appimageTools.wrapType2 {
       inherit version;
       name = "orca-slicer";
+
       src = pkgs.fetchurl {
         url =
           "https://github.com/SoftFever/OrcaSlicer/releases/download/v${version}/${name}.AppImage";
-        hash = "sha256-5Ag+pZBsOULp2Kk0KVG/P2ZQ/UVZm7zJZ4B2eXNeS50=";
+        hash = "sha256-neKWYzGbGed9awMoBQEwwZJKaeaV+5fdjM1MphjWkUg=";
       };
-      extraPkgs = pkgs: with pkgs; [ webkitgtk ];
+
+      profile = ''
+        export LC_ALL=C.UTF-8
+      '';
+
+      extraPkgs = pkgs:
+        (pkgs.appimageTools.defaultFhsEnvArgs.multiPkgs pkgs) ++ ([
+          pkgs.webkitgtk
+          pkgs.gst_all_1.gstreamer
+          pkgs.gst_all_1.gst-libav
+          pkgs.gst_all_1.gst-plugins-bad
+        ]);
     })
   ];
 }
