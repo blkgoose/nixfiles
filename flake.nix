280bf99e2301904a9615f52159b817f4d49d36a8
blkgoose <alessio.biancone@gmail.com>
2023-12-11 10:41:56 +0100

    chore: update to 23.11

--- flake.nix
+++ flake.nix
@@ -2,11 +2,11 @@
   description = "My NixOS flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-23.05";
+    nixpkgs.url = "github:nixos/nixpkgs/release-23.11";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.05";
+      url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
