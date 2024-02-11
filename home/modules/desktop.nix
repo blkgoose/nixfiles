58043f452166b8d69464debb9d95a450eb5cdf14
blkgoose <alessio.biancone@gmail.com>
2024-02-11 18:43:37 +0100

    feat: add wlsunset

--- home/modules/desktop.nix
+++ home/modules/desktop.nix
@@ -2,4 +2,10 @@
   home.packages = with pkgs; [ spotify feh gimp google-chrome ];
 
   home.sessionPath = [ "$HOME/.cargo/bin" ];
+
+  services.wlsunset = {
+    enable = true;
+    latitude = "42.50";
+    longitude = "12.50";
+  };
 }
