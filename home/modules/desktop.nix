0416fe6da5fca9a04a1a6250e6bb67baf1f71ba7
blkgoose <alessio.biancone@gmail.com>
2024-02-11 17:44:19 +0100

    feat: remove old picom and redshift

--- home/modules/desktop.nix
+++ home/modules/desktop.nix
@@ -2,45 +2,4 @@
   home.packages = with pkgs; [ spotify feh gimp xclip google-chrome ];
 
   home.sessionPath = [ "$HOME/.cargo/bin" ];
-
-  services.redshift = {
-    enable = true;
-    provider = "manual";
-    latitude = "45.4643";
-    longitude = "9.1895";
-    settings = {
-      redshift = {
-        brightness-day = 1;
-        brightness-night = 0.75;
-      };
-    };
-    temperature = {
-      day = 5500;
-      night = 3700;
-    };
-  };
-
-  services.picom = {
-    enable = true;
-    # backend = "glx";
-    vSync = true;
-    # fade = true;
-    # inactiveOpacity = 0.8;
-
-    # settings = {
-    #   blur = {
-    #     method = "dual_kawase";
-    #     strength = 7.0;
-    #     deviation = 1.0;
-    #     kernel = "11x11gaussian";
-    #   };
-    #   blur-background = false;
-    #   blur-background-frame = true;
-    #   blur-background-fixed = true;
-
-    #   blur-background-exclude = [ "class_g = 'Chrome'" "name = 'slop'" ];
-
-    #   corner-radius = 10.0;
-    # };
-  };
 }
