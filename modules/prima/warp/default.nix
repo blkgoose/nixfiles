b7d213e4c57ce9424ffbf3e5bf79221221cdf38c
blkgoose <alessio.biancone@gmail.com>
2024-02-19 14:52:39 +0100

    refactor: download vpn certificate directly

--- modules/prima/warp/default.nix
+++ modules/prima/warp/default.nix
@@ -1,7 +1,13 @@
-{ secret_dots, pkgs, ... }: {
+{ pkgs, ... }:
+let
+  cloudflare-ca = pkgs.fetchurl {
+    url =
+      "https://developers.cloudflare.com/cloudflare-one/static/Cloudflare_CA.pem";
+    sha256 = "sha256-7p2+Y657zy1TZAsOnZIKk+7haQ9myGTDukKdmupHVNX=";
+  };
+in {
   environment.systemPackages = with pkgs; [ cloudflare-warp ];
   systemd.packages = with pkgs; [ cloudflare-warp ];
 
-  security.pki.certificateFiles =
-    [ "${secret_dots}/vpn/warp/Cloudflare_CA.pem" ];
+  security.pki.certificateFiles = [ cloudflare-ca ];
 }
