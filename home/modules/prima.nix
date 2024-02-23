e5a20618b4165d1a51c2e9cc31f57290c6e278b7
blkgoose <alessio.biancone@gmail.com>
2024-02-23 14:52:11 +0100

    feat: add npmrc for prima

--- home/modules/prima.nix
+++ home/modules/prima.nix
@@ -1,1 +1,5 @@
-{ pkgs, ... }: { home.packages = with pkgs; [ suite_py ]; }
+{ pkgs, secret_dots, ... }: {
+  home.packages = with pkgs; [ suite_py ];
+
+  home.file.".npmrc".source = "${secret_dots}/npm/npmrc";
+}
