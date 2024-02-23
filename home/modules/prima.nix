654744d9b478c0252a6430649f67e7c7af1bf857
blkgoose <alessio.biancone@gmail.com>
2024-02-23 14:56:39 +0100

    feat: add suite_py to prima

--- home/modules/prima.nix
+++ home/modules/prima.nix
@@ -1,5 +1,8 @@
 { pkgs, secret_dots, ... }: {
   home.packages = with pkgs; [ suite_py ];
 
-  home.file.".npmrc".source = "${secret_dots}/npm/npmrc";
+  home.file = {
+    ".npmrc".source = "${secret_dots}/npm/npmrc";
+    ".suite_py/config.yml".source = "${secret_dots}/suite_py/config.yml";
+  };
 }
