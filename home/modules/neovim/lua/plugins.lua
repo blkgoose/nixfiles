366e9b224cf6420f68b7e1a6db176b15e85dc80f
blkgoose <alessio.biancone@gmail.com>
2024-06-03 17:26:06 +0200

    fix(nvim): use new interface for inlay-hints

--- home/modules/neovim/lua/plugins.lua
+++ home/modules/neovim/lua/plugins.lua
@@ -178,7 +178,7 @@ require("lazy").setup({
         callback = function(args)
           local client = vim.lsp.get_client_by_id(args.data.client_id)
           if client.server_capabilities.inlayHintProvider then
-            vim.lsp.inlay_hint.enable(args.buf, true)
+            vim.lsp.inlay_hint.enable(true)
           end
         end
       })
