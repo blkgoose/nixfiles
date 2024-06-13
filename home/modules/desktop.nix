4089dae5285177ff9c5bba3e851b57c23cd18f2d
blkgoose <alessio.biancone@gmail.com>
2024-06-13 11:31:00 +0200

    refactor: use discord web instead of app

--- home/modules/desktop.nix
+++ home/modules/desktop.nix
@@ -1,5 +1,5 @@
 { pkgs, ... }: {
-  home.packages = with pkgs; [ spotify feh gimp google-chrome discord ];
+  home.packages = with pkgs; [ spotify feh gimp google-chrome ];
 
   home.sessionPath = [ "$HOME/.cargo/bin" ];
 
