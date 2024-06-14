ceefb71d9d2dc57ed01e7d09a4182758be08fabb
blkgoose <alessio.biancone@gmail.com>
2024-06-14 15:16:30 +0200

    feat(prima-git): add local hook run after gitleaks

--- home/modules/prima-git.nix
+++ home/modules/prima-git.nix
@@ -10,6 +10,15 @@ in {
 
   xdg.configFile."git/hooks/pre-commit".source =
     pkgs.writers.writeBash "pre-commit" ''
+      PROJ_ROOT=$(git rev-parse --show-toplevel)
+      HOOK_PATH=$PROJ_ROOT/.git/hooks/pre-commit
+
       ${pkgs.gitleaks}/bin/gitleaks protect -c ${gitleaks_config} -v --staged --redact
+
+      if [ -e "$HOOK_PATH" ] && [ -x "$HOOK_PATH" ] ; then
+        $HOOK_PATH "$@"
+      else
+        exit 0
+      fi
     '';
 }
