1a14741f78588794aaf3a60bb9ea5a2fb1e13e88
blkgoose <alessio.biancone@gmail.com>
2024-10-04 09:25:35 +0200

    feat(tmux): set as default terminal multiplexer

--- home/modules/alacritty.nix
+++ home/modules/alacritty.nix
@@ -1,1 +1,11 @@
-{ pkgs, ... }: { home.packages = with pkgs; [ alacritty ]; }
+{ ... }: {
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      shell = {
+        program = "fish";
+        args = [ "-c" "tmux attach || tmux" ];
+      };
+    };
+  };
+}
