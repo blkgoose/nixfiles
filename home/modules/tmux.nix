c80273b463596242c2fdfb47a1ab1b354978f9ce
blkgoose <alessio.biancone@gmail.com>
2024-10-21 13:21:37 +0200

    feat(tmux): add bings

--- home/modules/tmux.nix
+++ home/modules/tmux.nix
@@ -17,29 +17,39 @@
     prefix = "C-Space";
 
     extraConfig = ''
-      bind -n m-H swap-pane -D
-      bind -n m-L swap-pane -U
+      bind v split-window -h
+      bind h split-window -v
+
+      bind -n M-h select-pane -L
+      bind -n M-j select-pane -D
+      bind -n M-k select-pane -U
+      bind -n M-l select-pane -R
+
+      bind -n M-H swap-pane -D
+      bind -n M-L swap-pane -U
 
       set -g detach-on-destroy off
 
-      bind-key -n 'M-r' source-file "${config.xdg.configHome}/tmux/tmux.conf" \; display "Configuration Reloaded!"
+      bind-key R source-file "${config.xdg.configHome}/tmux/tmux.conf" \; display "Configuration Reloaded!"
+
+      set -g status-justify centre
+      set -g status-style fg=colour180
 
       setw -g window-status-format " #I:#W "
       setw -g window-status-current-format " #I:#W "
-      setw -g window-status-current-style fg=colour211
+      setw -g window-status-current-style fg=colour211,bg=colour0
 
       bind -n 'M-,' previous-window
       bind -n 'M-.' next-window
       bind -n 'M-<' swap-window -t -1 \; previous-window
       bind -n 'M->' swap-window -t +1 \; next-window
 
-      bind -n 'M-b' break-pane \; next-window
-      bind -n 'M-j' choose-window 'join-pane -s "%%"'
+      bind b break-pane \; next-window
+      bind j choose-window 'join-pane -s "%%"'
 
       #sesh
       bind -n 'M-s' display-popup -E -w 40% "sesh connect \"$( sesh list -i | fzf --ansi )\""
       bind -n 'M-S' run-shell 'sesh last'
-      bind -n 'M-k' kill-session
     '';
   };
 }
