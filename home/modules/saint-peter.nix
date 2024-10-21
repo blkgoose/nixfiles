2cf0b0b9d04a9af9eff7703c63546aa8bb842507
blkgoose <alessio.biancone@gmail.com>
2024-10-21 13:32:52 +0200

    feat(saint-peter): add config

--- home/modules/saint-peter.nix
+++ home/modules/saint-peter.nix
@@ -2,4 +2,11 @@
   xdg.configFile = {
     "saint-peter.json".source = "${secret_dots}/saint-peter/saint-peter.json";
   };
+
+  home.file.".ssh/config".source = ''
+    Host github.com
+       HostName github.com
+       User git
+       IdentityFile ~/.ssh/SAINT_PETER_GIT_KEY
+  '';
 }
