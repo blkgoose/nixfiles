794cb0316d6ba544b948448e6198c9b4d572b73f
blkgoose <alessio.biancone@gmail.com>
2024-10-22 18:08:08 +0200

    fix: wrap picom in nixgl

--- home/modules/xmonad/picom.nix
+++ home/modules/xmonad/picom.nix
@@ -1,6 +1,23 @@
-{ pkgs, ... }: {
-  home.packages = [ pkgs.picom ];
+{ pkgs, ... }:
+let
+  wrapNixGL = package:
+    (pkgs.symlinkJoin {
+      name = package.name;
+      paths = [ package ];
+      buildInputs = [ pkgs.makeWrapper ];
+      postBuild = ''
+        for full_path in $out/bin/*; do
+          f=$(basename $full_path)
+          rm $out/bin/$f
+          echo "#!${pkgs.runtimeShell}" > $out/bin/$f
+          echo "exec ${pkgs.nixgl.nixGLIntel}/bin/nixGLIntel ${package}/bin/$f" '"$@"' >> $out/bin/$f
+          chmod +x $out/bin/$f
+        done
+      '';
+    });
 
+  picom = wrapNixGL pkgs.picom;
+in {
   xdg.configFile."picom/picom.conf" = {
     text = ''
       vsync = true;
@@ -13,7 +30,7 @@
 
   systemd.user.services."picom" = {
     Unit.Description = "Runs picom correctly";
-    Service.ExecStart = "${pkgs.picom}/bin/picom";
+    Service.ExecStart = "${picom}/bin/picom";
     Install.WantedBy = [ "graphical-session.target" ];
 
     Unit = {
