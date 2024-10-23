2e1f2d9249d693ff52a9a90c6b2db76aa50cdf24
blkgoose <alessio.biancone@gmail.com>
2024-10-23 08:53:15 +0200

    refactor: use wrapped alacritty directly

--- home/systems/shittop.nix
+++ home/systems/shittop.nix
@@ -1,7 +1,5 @@
 { pkgs, ... }: {
   imports = [ ../users/prima.nix ../modules/docker.nix ];
-  # overrides
-  programs.alacritty.package = pkgs.emptyDirectory; # installed by system
+  programs.alacritty.package = with pkgs; nixGL alacritty;
 
-  home.packages = with pkgs; [ (nixGL alacritty) ];
 }
