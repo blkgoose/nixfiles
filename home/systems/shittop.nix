57eea6b19701ef600c38013eb5e66845af9ed989
blkgoose <alessio.biancone@gmail.com>
2024-10-23 08:53:35 +0200

    feat: add sane keyboard configuration

--- home/systems/shittop.nix
+++ home/systems/shittop.nix
@@ -2,4 +2,8 @@
   imports = [ ../users/prima.nix ../modules/docker.nix ];
   programs.alacritty.package = with pkgs; nixGL alacritty;
 
+  xsession.numlock.enable = false;
+  xsession.initExtra = ''
+    ${pkgs.xorg.setxkbmap}/bin/setxkbmap -option compose:ralt
+  '';
 }
