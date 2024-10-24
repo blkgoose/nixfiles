05278ca93d59e80c0c31abb4678c16a301cc414f
blkgoose <alessio.biancone@gmail.com>
2024-10-24 21:16:23 +0200

    feat(xmonad): update interface

--- home/modules/xmonad/xmobar.nix
+++ home/modules/xmonad/xmobar.nix
@@ -2,14 +2,30 @@
   programs.xmobar = {
     enable = true;
     extraConfig = ''
-      Config { template = "%cpu% | %memory% * %swap% }{<fc=#ee9a00>%date%</fc>"
+      Config { template = "    %XMonadLog% } %time% {%battery% %cpu% %memory%    "
              , bgColor = "#212121"
              , fgColor = "#F5F5F5"
-             , position = TopHM 50 10 10 10 0
-             , commands = [ Run Cpu ["-L","3","-H","50","--normal","green","--high","red"] 10
-                          , Run Memory ["-t","Mem: <usedratio>%"] 10
-                          , Run Swap [] 10
-                          , Run Date "%a %b %_d %Y %H:%M:%S" "date" 10
+             , font = "RobotoMono Nerd Font"
+             , position = TopHM 40 10 10 10 0
+             , commands = [ Run Cpu [ "--template", " <total>%"
+                                    , "-L", "3"
+                                    , "-H", "50"
+                                    , "--high", "red"
+                                    ] 10
+                          , Run Memory [ "--template", " <usedratio>%"
+                                       , "--high", "red"
+                                       ] 10
+                          , Run Battery [ "--template", "<acstatus>"
+                                        , "--Low", "10"
+                                        , "--High", "80"
+                                        , "--low", "red"
+                                        , "--"
+                                        , "-o", "󰁹 <left>% (<timeleft>)"
+                                        , "-O", "󰁹 <left>%+ (<timeleft>)"
+                                        , "-i", ""
+                                        ] 10
+                          , Run Date "%H:%M:%S" "time" 10
+                          , Run XMonadLog
                           ]
              , sepChar = "%"
              , alignSep = "}{"
