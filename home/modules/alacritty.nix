b23bbbb078369289e2000df56d9741fe68cb84fe
blkgoose <alessio.biancone@gmail.com>
2024-10-24 20:57:42 +0200

    feat: use roboto mono

--- home/modules/alacritty.nix
+++ home/modules/alacritty.nix
@@ -1,1 +1,15 @@
-{ ... }: { programs.alacritty = { enable = true; }; }
+{ pkgs, ... }: {
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      font = {
+        normal = {
+          family = "RobotoMono Nerd Font";
+          style = "Regular";
+        };
+        size = 11.0;
+      };
+      shell.program = "${pkgs.fish}/bin/fish";
+    };
+  };
+}
