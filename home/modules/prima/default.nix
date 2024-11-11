3cd37e75c08aed228f675c392f27e673dfa19592
blkgoose <alessio.biancone@gmail.com>
2024-11-11 10:12:24 +0100

    feat: add cloudflare-warp

--- home/modules/prima/default.nix
+++ home/modules/prima/default.nix
@@ -1,7 +1,7 @@
 { pkgs, secret_dots, ... }: {
   imports = [ ./git.nix ];
 
-  home.packages = with pkgs; [ suite_py ];
+  home.packages = with pkgs; [ suite_py cloudflare-warp ];
 
   home.file = {
     ".npmrc".source = "${secret_dots}/npm/npmrc";
