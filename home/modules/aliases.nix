24b26005e33ea2561c273ca64d92fb1d0b1ae67c
blkgoose <alessio.biancone@gmail.com>
2024-11-21 14:53:29 +0100

    refactor: move alias in DE specific folder

--- home/modules/aliases.nix
+++ home/modules/aliases.nix
@@ -1,26 +1,10 @@
 { lib, pkgs, ... }:
+# DEPRECATED. USE `pkgs.writeShellApplication` INSTEAD
 let
   apps = {
-    "record" = "${pkgs.kooha}/bin/kooha";
-    "screenshot" =
-      "${pkgs.unstable.hyprshot}/bin/hyprshot --mode region --clipboard-only --silent";
     "chrome" = "${pkgs.google-chrome}/bin/google-chrome-stable";
     "beekeeper" = "${pkgs.beekeeper-studio}/bin/beekeeper-studio";
     "zoom" = "${pkgs.zoom-us}/bin/zoom";
-    "lock" = ''
-      ${pkgs.swaylock-effects}/bin/swaylock \
-            --screenshot \
-            --clock \
-            --indicator \
-            --effect-blur 7x5 \
-            --grace 2 \
-            --fade-in 0.2
-    '';
-    "mirror" = ''
-      ${pkgs.wdomirror}/bin/wdomirror $(${pkgs.wdomirror}/bin/wdomirror  \
-            | ${pkgs.gnugrep}/bin/grep -oP '(?<=ID: )\d+'  \
-            | ${pkgs.coreutils}/bin/head -1) \
-    '';
   };
 
   mkAlias = name: command: {
--- home/modules/xmonad/default.nix
+++ home/modules/xmonad/default.nix
@@ -1,8 +1,22 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+let
+  alias = name: command:
+    pkgs.writeShellApplication {
+      inherit name;
+      text = ''
+        ${command} "$@"
+      '';
+    };
+in {
   imports =
     [ ./xmonad.nix ./xmobar.nix ./picom.nix ./dunst.nix ./wallpaper.nix ];
 
-  home.packages = with pkgs; [ autorandr pavucontrol ];
+  home.packages = with pkgs; [
+    autorandr
+    pavucontrol
+    (alias "screenshot" "${escrotum}/bin/escrotum --select --clipboard")
+    (alias "lock" "${sloc}/bin/slock")
+  ];
 
   systemd.user.services."keyboard-configurations" = {
     Service = {
