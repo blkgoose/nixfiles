f7925dc1e55e4f33356eef4485b5e600f4f94c56
blkgoose <alessio.biancone@gmail.com>
2024-11-21 14:53:28 +0100

    refactor: move systemconfig to own folder

--- flake.nix
+++ flake.nix
@@ -95,47 +95,14 @@
         };
       };
 
-      systemConfigs.default = system-manager.lib.makeSystemConfig {
-        modules = [
-          ({ ... }:
-            let
-              pkgs = import inputs.nixpkgs {
-                inherit system;
-                config.allowUnfree = true;
-              };
-
-              cloudflare-ca = pkgs.fetchurl {
-                url =
-                  "https://developers.cloudflare.com/cloudflare-one/static/Cloudflare_CA.pem";
-                sha256 = "sha256-7p2+Y657zy1TZAsOnZIKk+7haQ9myGTDukKdmupHVNX=";
-              };
-            in {
-              nixpkgs.hostPlatform = "x86_64-linux";
-
-              environment.etc."ssl/certs/cloudflare.crt".source = cloudflare-ca;
-
-              systemd.services.dockerd = {
-                enable = true;
-                serviceConfig = {
-                  Type = "notify";
-                  ExecStart = "${pkgs.docker}/bin/dockerd";
-                };
-                description = "Runs docker daemon";
-                after = [ "network.target" ];
-                wantedBy = [ "multi-user.target" ];
-              };
-
-              systemd.services.warp-svc = {
-                enable = true;
-                serviceConfig = {
-                  ExecStart = "${pkgs.cloudflare-warp}/bin/warp-svc";
-                };
-                description = "Runs warp daemon";
-                after = [ "network.target" ];
-                wantedBy = [ "multi-user.target" ];
-              };
-            })
-        ];
+      systemConfigs = {
+        "shittop" = system-manager.lib.makeSystemConfig {
+          extraSpecialArgs = inputs // {
+            inherit system;
+            inherit pkgs;
+          };
+          modules = [ ./systems/shittop ];
+        };
       };
     };
 }
