9419d036352ce357b43ce5ec16d7ef44c1567aac
blkgoose <alessio.biancone@gmail.com>
2024-11-21 14:53:28 +0100

    feat: add orca-slicer

--- home/modules/desktop.nix
+++ home/modules/desktop.nix
@@ -1,5 +1,12 @@
 { pkgs, ... }: {
-  home.packages = with pkgs; [ spotify feh gimp google-chrome discord ];
+  home.packages = with pkgs; [
+    spotify
+    feh
+    gimp
+    google-chrome
+    discord
+    orca-slicer
+  ];
 
   home.sessionPath = [ "$HOME/.cargo/bin" ];
 }
