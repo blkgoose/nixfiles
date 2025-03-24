316343482f926fb866ebfe7006a4debece625478
blkgoose <alessio.biancone@gmail.com>
2025-03-24 09:25:32 +0100

    feat: make ssh key usable by sub processes

--- home/modules/saint-peter.nix
+++ home/modules/saint-peter.nix
@@ -3,12 +3,17 @@
     "saint-peter.json".source = "${secret_dots}/saint-peter/saint-peter.json";
   };
 
-  home.file.".ssh/config" = {
+  # hack to make the config file available for devcontainer that doesn't have access to symlink
+  home.file.".ssh/config_link" = {
     text = ''
       Host github.com
          HostName github.com
          User git
          IdentityFile ~/.ssh/SAINT_PETER_GIT_KEY
     '';
+    onChange = ''
+      cp ~/.ssh/config_link ~/.ssh/config
+    '';
   };
+
 }
