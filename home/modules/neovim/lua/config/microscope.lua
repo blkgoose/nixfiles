1d1aa2f98fc25c3f130f8b73d383929fa4999f65
blkgoose <alessio.biancone@gmail.com>
2025-04-18 16:00:55 +0200

    style: formatted

--- home/modules/neovim/lua/config/microscope.lua
+++ home/modules/neovim/lua/config/microscope.lua
@@ -9,79 +9,79 @@ local code = require("microscope-code")
 local display = require("microscope.api.display")
 local builtin = require("microscope.builtin.layouts")
 
-files_utils.max_size = 2^18;
+files_utils.max_size = 2 ^ 18;
 
 local layout_list = {
-  function(opts)
-    if opts.full_screen then
-      return display
-        .horizontal({
-          display.preview(),
-          display.vertical({
-            display.input(1),
-            display.results(5),
-            display.space(),
-          }),
-        })
-        :build(opts.ui_size)
-    else
-      return display
-        .horizontal({
-          display.vertical({
-            display.input(1),
-            display.results(5),
-            display.space(),
-          }),
-          display.preview(),
-        })
-        :build(opts.ui_size)
-    end
-  end,
-  builtin.default,
-  function(opts)
-    return display
-      .horizontal({
-        display.space("20%"),
-        display.vertical({
-          display.input(1),
-          display.results(5),
-          display.space(),
-        }),
-        display.space("20%"),
-      })
-      :build(opts.ui_size)
-  end,
+    function(opts)
+        if opts.full_screen then
+            return display
+                .horizontal({
+                    display.preview(),
+                    display.vertical({
+                        display.input(1),
+                        display.results(5),
+                        display.space(),
+                    }),
+                })
+                :build(opts.ui_size)
+        else
+            return display
+                .horizontal({
+                    display.vertical({
+                        display.input(1),
+                        display.results(5),
+                        display.space(),
+                    }),
+                    display.preview(),
+                })
+                :build(opts.ui_size)
+        end
+    end,
+    builtin.default,
+    function(opts)
+        return display
+            .horizontal({
+                display.space("20%"),
+                display.vertical({
+                    display.input(1),
+                    display.results(5),
+                    display.space(),
+                }),
+                display.space("20%"),
+            })
+            :build(opts.ui_size)
+    end,
 }
 local function rotate_layout(instance)
-  instance:alter(function(opts)
-    opts.mode = opts.mode or 1
-    opts.layout = layout_list[opts.mode + 1]
-    opts.mode = (opts.mode + 1) % #layout_list
-    return opts
-  end)
+    instance:alter(function(opts)
+        opts.mode = opts.mode or 1
+        opts.layout = layout_list[opts.mode + 1]
+        opts.mode = (opts.mode + 1) % #layout_list
+        return opts
+    end)
 end
 
 microscope.setup({
-  layout = layout_list[1],
-  prompt = "> ",
-  size = {
-    width = 125,
-    height = 40,
-  },
-  bindings = {
-    ["<c-j>"] = actions.next,
-    ["<c-k>"] = actions.previous,
-    ["<c-n>"] = actions.scroll_down,
-    ["<c-p>"] = actions.scroll_up,
-    ["<c-m>"] = actions.toggle_full_screen,
-    ["<c-q>"] = files.actions.quickfix,
-    ["<c-h>"] = actions.hide,
-    ["<c-a>"] = rotate_layout,
-    ["<a-cr>"] = actions.refine,
-    ["<CR>"] = actions.open,
-    ["<ESC>"] = actions.close,
-    ["<TAB>"] = actions.select,
-  },
+    layout = layout_list[1],
+    prompt = "> ",
+    size = {
+        width = 125,
+        height = 40,
+    },
+    bindings = {
+        ["<c-j>"] = actions.next,
+        ["<c-k>"] = actions.previous,
+        ["<c-n>"] = actions.scroll_down,
+        ["<c-p>"] = actions.scroll_up,
+        ["<c-m>"] = actions.toggle_full_screen,
+        ["<c-q>"] = files.actions.quickfix,
+        ["<c-h>"] = actions.hide,
+        ["<c-a>"] = rotate_layout,
+        ["<a-cr>"] = actions.refine,
+        ["<CR>"] = actions.open,
+        ["<ESC>"] = actions.close,
+        ["<TAB>"] = actions.select,
+    },
 })
 
 microscope.register(files.finders)
