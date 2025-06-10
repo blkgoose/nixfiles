d470a7bac0e9b62efa39fe33aae7480504816d61
blkgoose <alessio.biancone@gmail.com>
2025-06-10 16:49:38 +0200

    feat(fish): add pnpm autocomplete

--- home/modules/fish/default.nix
+++ home/modules/fish/default.nix
@@ -1,4 +1,27 @@
 { pkgs, lib, ... }: {
+  xdg.configFile."fish/completions/pnpm.fish".source = pkgs.writeText "pnpm" ''
+    function _pnpm_completion
+      set cmd (commandline -o)
+      set cursor (commandline -C)
+      set words (count $cmd)
+
+      set completions (eval env DEBUG=\"" \"" COMP_CWORD=\""$words\"" COMP_LINE=\""$cmd \"" COMP_POINT=\""$cursor\"" SHELL=fish pnpm completion-server -- $cmd)
+
+      if [ "$completions" = "__tabtab_complete_files__" ]
+        set -l matches (commandline -ct)*
+        if [ -n "$matches" ]
+          __fish_complete_path (commandline -ct)
+        end
+      else
+        for completion in $completions
+          echo -e $completion
+        end
+      end
+    end
+
+    complete -f -d 'pnpm' -c pnpm -a "(_pnpm_completion)"
+  '';
+
   programs.fish = {
     enable = true;
 
