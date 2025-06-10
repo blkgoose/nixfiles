50fe855dd0641c881739495e2a818cff634ef96e
blkgoose <alessio.biancone@gmail.com>
2025-06-10 09:17:45 +0200

    feat: rename legcord with discord

--- home/modules/desktop.nix
+++ home/modules/desktop.nix
@@ -6,7 +6,7 @@
     beekeeper-studio
     zoom
     (alias "chrome" "${(nixGL google-chrome)}/bin/google-chrome-stable")
-    (alias "legcord" "${(nixGL legcord)}/bin/legcord")
+    (alias "discord" "${(nixGL legcord)}/bin/legcord")
   ];
 
   home.sessionPath = [ "$HOME/.cargo/bin" ];
