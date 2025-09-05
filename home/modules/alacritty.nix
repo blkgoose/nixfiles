ddc74a72064d631f78bfb8f9fad1d5b757793e08
blkgoose <alessio.biancone@gmail.com>
2025-09-05 20:40:03 +0200

    fix: make nerd-fonts work

--- home/modules/alacritty.nix
+++ home/modules/alacritty.nix
@@ -4,9 +4,10 @@
     settings = {
       font = {
         normal = {
-          family = "monospace";
+          family = "RobotoMono Nerd Font Mono";
           style = "Regular";
         };
+        size = 8.0;
       };
       terminal.shell.program = "${pkgs.fish}/bin/fish";
     };
