1b16b09cf47e6e617b60b52557d369eb6f6595b6
blkgoose <alessio.biancone@gmail.com>
2025-09-06 00:56:07 +0200

    chore: alacritty font 11

--- home/modules/alacritty.nix
+++ home/modules/alacritty.nix
@@ -7,7 +7,7 @@
           family = "RobotoMono Nerd Font Mono";
           style = "Regular";
         };
-        size = 8.0;
+        size = 11.0;
       };
       terminal.shell.program = "${pkgs.fish}/bin/fish";
     };
