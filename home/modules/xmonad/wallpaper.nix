1e09b786b9e6daa53bf2ffd9ed3fd3c01a304952
blkgoose <alessio.biancone@gmail.com>
2025-09-25 09:11:21 +0200

    Revert "feat: refresh wallpaper every 5 sec"
    This reverts commit f8ba288b3966e4dd1609df8c54fe4b255de61d75.

--- home/modules/xmonad/wallpaper.nix
+++ home/modules/xmonad/wallpaper.nix
@@ -1,12 +1,7 @@
 { pkgs, ... }: {
   systemd.user.services.wallpaper = {
     Unit.Description = "Sets wallpaper";
-    Service.ExecStart = pkgs.writers.writeBash "wallpaper-setter" ''
-      while true; do
-          ${pkgs.feh}/bin/feh --bg-fill ${./wallpaper}
-          sleep 5
-      done
-    '';
+    Service.ExecStart = "${pkgs.feh}/bin/feh --bg-fill ${./wallpaper}";
     Install.WantedBy = [ "graphical-session.target" ];
 
     Unit = {
