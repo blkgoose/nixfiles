bddae7a441f5859683464de76fc5193d16238e96
blkgoose <alessio.biancone@gmail.com>
2025-10-09 14:32:00 +0200

    Revert "chore: remove earlyOOM"
    This reverts commit f92fa4ccb05df651b4496cfedb115a3e84402913.

--- systems/shittop/default.nix
+++ systems/shittop/default.nix
@@ -32,6 +32,13 @@ in {
     wantedBy = [ "multi-user.target" ];
   };
 
+  systemd.services.earlyoom = {
+    enable = true;
+    serviceConfig = { ExecStart = "${pkgs.earlyoom}/bin/earlyoom"; };
+    description = "Runs earlyOOM daemon";
+    wantedBy = [ "multi-user.target" ];
+  };
+
   systemd.services.bigswap = {
     enable = true;
     serviceConfig = {
