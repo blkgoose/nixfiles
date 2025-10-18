ce2da392c6bf3429841cf6f407487cb85cccf516
blkgoose <alessio.biancone@gmail.com>
2025-10-18 15:25:31 +0200

    refactor: use variable instead of hardcoded path

--- home/modules/neovim/default.nix
+++ home/modules/neovim/default.nix
@@ -1,6 +1,6 @@
 { pkgs, config, ... }: {
   xdg.configFile."nvim".source = config.lib.file.mkOutOfStoreSymlink
-    "/home/alessiobiancone/.config/nix/home/modules/neovim";
+    "${config.xdg.configHome}/nix/home/modules/neovim";
 
   programs.neovim = {
     package = pkgs.unstable.neovim-unwrapped;
