8fdfbc0f1784cce0639347af29147f97f8fe4cb4
blkgoose <alessio.biancone@gmail.com>
2025-10-18 15:25:31 +0200

    fix(hyprland): remove deprecated options

--- home/modules/hyprland/hyprland.nix
+++ home/modules/hyprland/hyprland.nix
@@ -52,11 +52,6 @@
 
         layerrule =
           [ "blur, waybar|swaync|wofi" "ignorezero, waybar|swaync|wofi" ];
-
-        drop_shadow = "yes";
-        shadow_range = 4;
-        shadow_render_power = 3;
-        "col.shadow" = "rgba(1a1a1aee)";
       };
 
       animations = {
