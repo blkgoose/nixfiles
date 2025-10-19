07300df92d39bb5ce65069517651fca5479e056f
blkgoose <alessio.biancone@gmail.com>
2025-10-19 18:25:05 +0200

    fix: set cursor

--- home/modules/hyprland/hyprland.nix
+++ home/modules/hyprland/hyprland.nix
@@ -16,6 +16,7 @@
         "systemctl --user start waybar"
         "dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
         "${pkgs.wayland-pipewire-idle-inhibit}/bin/wayland-pipewire-idle-inhibit"
+        "hyprctl setcursor Adwaita 24"
       ];
 
       env = [ "PATH, ${config.home.homeDirectory}/.local/bin:$PATH" ];
