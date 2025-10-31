4d753ecde8dda08be5d6a4c6ccb79275e6db51bb
blkgoose <alessio.biancone@gmail.com>
2025-10-31 15:55:45 +0100

    refactor: use out of store symlink for configurations

--- home/modules/prima/default.nix
+++ home/modules/prima/default.nix
@@ -1,7 +1,9 @@
-{ pkgs, prima-nix, secret_dots, ... }:
+{ pkgs, prima-nix, secret_dots, config, ... }:
 let
   vault_addr = builtins.head (builtins.split "\n"
     (builtins.readFile "${secret_dots}/vault/prima_address"));
+
+  secrets = "${config.xdg.configHome}/nix/secret_dotfiles/";
 in {
   imports = [ prima-nix.homeManagerModules.gitleaks ];
 
@@ -15,10 +17,14 @@ in {
   ];
 
   home.file = {
-    ".npmrc".source = "${secret_dots}/npm/npmrc";
-    ".cargo/credentials.toml".source = "${secret_dots}/cargo/credentials.toml";
-    ".suite_py/config.yml".source = "${secret_dots}/suite_py/config.yml";
-    ".config/starscli/config.yml".source = "${secret_dots}/starscli/config.yml";
+    ".npmrc".source =
+      config.lib.file.mkOutOfStoreSymlink "${secrets}/npm/npmrc";
+    ".cargo/credentials.toml".source =
+      config.lib.file.mkOutOfStoreSymlink "${secrets}/cargo/credentials.toml";
+    ".suite_py/config.yml".source =
+      config.lib.file.mkOutOfStoreSymlink "${secrets}/suite_py/config.yml";
+    ".config/starscli/config.yaml".source =
+      config.lib.file.mkOutOfStoreSymlink "${secrets}/starscli/config.yaml";
   };
 
   prima.gitleaks.enable = true;
