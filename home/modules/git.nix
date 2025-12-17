05266ed9e13f83705990a00119b506c054aab001
blkgoose <alessio.biancone@gmail.com>
2025-12-17 10:17:38 +0100

    chore: update git to new version

--- home/modules/git.nix
+++ home/modules/git.nix
@@ -4,34 +4,36 @@
 
     ignores = [ ".direnv/" "mutagen.yaml.lock" ];
 
-    aliases = {
-      lg =
-        "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ad)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)'";
-      s = "status -sb";
-      unstage = "reset HEAD --";
-      yolo = "push --force-with-lease";
-      fap = "fetch --all --prune";
-    };
+    settings = {
+      aliases = {
+        lg =
+          "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ad)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)'";
+        s = "status -sb";
+        unstage = "reset HEAD --";
+        yolo = "push --force-with-lease";
+        fap = "fetch --all --prune";
+      };
 
-    extraConfig = {
-      user = {
-        name = "blkgoose";
-        email = "alessio.biancone@gmail.com";
+      extraConfig = {
+        user = {
+          name = "blkgoose";
+          email = "alessio.biancone@gmail.com";
+        };
+        init = { defaultBranch = "master"; };
+        credential.helper = "store";
+        core = {
+          editor = "vim";
+          autocrlf = "input";
+          filemode = false;
+        };
+        push = {
+          followTags = true;
+          autoSetupRemote = true;
+        };
+        diff.tool = "vim -d";
+        difftool.prompt = false;
+        merge.tool = "vim -d";
       };
-      init = { defaultBranch = "master"; };
-      credential.helper = "store";
-      core = {
-        editor = "vim";
-        autocrlf = "input";
-        filemode = false;
-      };
-      push = {
-        followTags = true;
-        autoSetupRemote = true;
-      };
-      diff.tool = "vim -d";
-      difftool.prompt = false;
-      merge.tool = "vim -d";
     };
   };
 }
