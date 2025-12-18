46e0fa915ce0cb634c3ca94e87fe346c977ccc03
blkgoose <alessio.biancone@gmail.com>
2025-12-18 16:59:44 +0100

    fix: use correct git alias name

--- home/modules/git.nix
+++ home/modules/git.nix
@@ -5,7 +5,7 @@
     ignores = [ ".direnv/" "mutagen.yaml.lock" ];
 
     settings = {
-      aliases = {
+      alias = {
         lg =
           "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ad)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)'";
         s = "status -sb";
