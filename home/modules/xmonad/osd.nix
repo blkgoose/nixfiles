185b9b0ddefbe7f4a3236ea649d57185ad2a346b
blkgoose <alessio.biancone@gmail.com>
2026-01-08 18:31:13 +0100

    fix: disable osd polling

--- home/modules/xmonad/osd.nix
+++ home/modules/xmonad/osd.nix
@@ -9,31 +9,31 @@
         path = "/sys/class/backlight/intel_backlight/brightness";
         max = 19393;
       };
-      "volume" = {
-        source = "poll";
-        command = ''
-          ${pkgs.wireplumber}/bin/wpctl get-volume @DEFAULT_AUDIO_SINK@ |
-          ${pkgs.coreutils}/bin/cut -d ':' -f2 |
-          ${pkgs.findutils}/bin/xargs |
-          ${pkgs.coreutils}/bin/tr -d '\n' |
-          ${pkgs.coreutils}/bin/tr -d '.'
-        '';
-      };
-      "volume:spotify" = {
-        source = "poll";
-        command = ''
-          ${pkgs.wireplumber}/bin/wpctl get-volume $(
-            ${pkgs.wireplumber}/bin/wpctl status |
-            ${pkgs.gnugrep}/bin/grep 'Streams' -A 10 |
-            ${pkgs.gnugrep}/bin/grep 'spotify' |
-            ${pkgs.coreutils}/bin/cut -d'.' -f1
-          ) |
-          ${pkgs.coreutils}/bin/cut -d ':' -f2 |
-          ${pkgs.findutils}/bin/xargs |
-          ${pkgs.coreutils}/bin/tr -d '\n' |
-          ${pkgs.coreutils}/bin/tr -d '.'
-        '';
-      };
+      # "volume" = {
+      #   source = "poll";
+      #   command = ''
+      #     ${pkgs.wireplumber}/bin/wpctl get-volume @DEFAULT_AUDIO_SINK@ |
+      #     ${pkgs.coreutils}/bin/cut -d ':' -f2 |
+      #     ${pkgs.findutils}/bin/xargs |
+      #     ${pkgs.coreutils}/bin/tr -d '\n' |
+      #     ${pkgs.coreutils}/bin/tr -d '.'
+      #   '';
+      # };
+      # "volume:spotify" = {
+      #   source = "poll";
+      #   command = ''
+      #     ${pkgs.wireplumber}/bin/wpctl get-volume $(
+      #       ${pkgs.wireplumber}/bin/wpctl status |
+      #       ${pkgs.gnugrep}/bin/grep 'Streams' -A 10 |
+      #       ${pkgs.gnugrep}/bin/grep 'spotify' |
+      #       ${pkgs.coreutils}/bin/cut -d'.' -f1
+      #     ) |
+      #     ${pkgs.coreutils}/bin/cut -d ':' -f2 |
+      #     ${pkgs.findutils}/bin/xargs |
+      #     ${pkgs.coreutils}/bin/tr -d '\n' |
+      #     ${pkgs.coreutils}/bin/tr -d '.'
+      #   '';
+      # };
     };
   };
 }
