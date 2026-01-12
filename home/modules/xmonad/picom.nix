38eccf5d0d86d9e2473531c25ea8a8d30dd6286d
blkgoose <alessio.biancone@gmail.com>
2026-01-12 09:49:18 +0100

    refactor(picom): use hm-service instead

--- home/modules/xmonad/picom.nix
+++ home/modules/xmonad/picom.nix
@@ -1,22 +1,11 @@
-{ pkgs, ... }: {
-  xdg.configFile."picom/picom.conf" = {
-    text = ''
-      vsync = true;
+{ ... }: {
+  services.picom = {
+    enable = true;
+    settings = {
       backend = "glx";
+      vsync = true;
       corner-radius = 20;
-      round-borders = 1;
-    '';
-    onChange = "${pkgs.systemd}/bin/systemctl --user restart picom";
-  };
-
-  systemd.user.services.picom = {
-    Unit.Description = "Runs picom correctly";
-    Service.ExecStart = "${pkgs.picom}/bin/picom";
-    Install.WantedBy = [ "graphical-session.target" ];
-
-    Unit = {
-      After = "graphical-session.target";
-      PartOf = "graphical-session.target";
+      round-borders = true;
     };
   };
 }
