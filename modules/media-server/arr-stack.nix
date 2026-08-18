b800ced0c7e9e476d6b453d3a8691cc9cfca4e16
blkgoose <alessio.biancone@gmail.com>
2026-08-18 16:34:37 +0200

    chore(jellyfin): added auto collections

--- modules/media-server/arr-stack.nix
+++ modules/media-server/arr-stack.nix
@@ -1,9 +1,24 @@
 { pkgs, ... }: {
   environment.etc = {
     "arr/docker-compose.yml".source = ./docker-compose.yml;
+    "arr/config/jellyfin-auto-collections/config.yaml".text = ''
+      crontab: !ENV ''${CRONTAB}
+      timezone: !ENV ''${TZ}
+      jellyfin:
+        server_url: !ENV ''${JELLYFIN_SERVER_URL}
+        api_key: !ENV ''${JELLYFIN_API_KEY}
+        user_id: !ENV ''${JELLYFIN_USER_ID}
+      plugins:
+        letterboxd:
+          enabled: true
+          list_ids:
+            - arinbicer/list/mcu
+    '';
     "arr/.env".source = pkgs.writeText "env" ''
       NORDVPN_USER=${pkgs.secret "nordvpn/username"}
       NORDVPN_PASS=${pkgs.secret "nordvpn/password"}
+      JELLYFIN_API_KEY=${pkgs.secret "jellyfin/api_key"}
+      JELLYFIN_USER_ID=${pkgs.secret "jellyfin/user_id"}
     '';
   };
 
